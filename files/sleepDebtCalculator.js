aa614081a6e853dba5d2e0f895e2007c37b75fd0
mkapusciok <m.kapusciok@interia.pl>
2022-12-28 21:23:05 +0100

    Create getActualSleepHours function

--- files/sleepDebtCalculator.js
+++ files/sleepDebtCalculator.js
@@ -1,17 +1,22 @@
 const getSleepHours = (day) => {
-    if (day === 'monday') {
-        return 6;
-    } else if (day === 'tuesday') {
-        return 9;
-    } else if (day === 'wednesday') {
-        return 7;
-    } else if (day === 'thursday') {
-        return 6;
-    } else if (day === 'friday') {
-        return 7;
-    } else if (day === 'saturday') {
-        return 5;
-    } else if (day === 'sunday') {
-        return 4;
-    }
+  if (day === "monday") {
+    return 6;
+  } else if (day === "tuesday") {
+    return 9;
+  } else if (day === "wednesday") {
+    return 7;
+  } else if (day === "thursday") {
+    return 6;
+  } else if (day === "friday") {
+    return 7;
+  } else if (day === "saturday") {
+    return 5;
+  } else if (day === "sunday") {
+    return 4;
+  }
+};
+
+const getActualSleepHours = () => {
+    getActualSleepHours = getSleepHours ('monday') + getSleepHours('tuesday') + getActualSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday');
+    return getActualSleepHours;
 };
